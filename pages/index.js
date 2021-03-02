6e601a463c3d652a404e7693d30f683bc796638a
juliusyam <juliusyam@users.noreply.github.com>
2021-03-02 19:37:03 +0000

    more hello world

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Fast Feedback</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          This is <a href="https://juliusyam.com">Fast Feedback</a>
         </h1>
 
         <p className={styles.description}>
