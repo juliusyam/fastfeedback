71b44b76093f71969514f59d6a1dce2b10dd9f8a
juliusyam <juliusyam@users.noreply.github.com>
2021-04-19 15:41:26 +0100

    ability to see site basic info

--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,19 +1,26 @@
 import { useRef, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Box, FormControl, FormLabel, Input, Button, useToast } from '@chakra-ui/react';
+import { Box, FormControl, FormLabel, Input, Button, useToast, Text, Stack, Flex, Avatar } from '@chakra-ui/react';
 import { getAllFeedback, getAllSites } from '@/lib/database-admin';
 import Feedback from '../../components/Feedback';
 import { useAuth } from '@/lib/auth';
 import dayjs from 'dayjs';
-import { createFeedback } from '@/lib/database';
+import { createFeedback, getAuthorInfo, getSiteInfo } from '@/lib/database';
+import NextLink from 'next/link';
 
 export async function getStaticProps(context) {
   const siteId = context.params.siteId;
   const { feedback } = await getAllFeedback(siteId);
+  const sites = await getSiteInfo(siteId);
+  
+  const authorId = sites.authorId;
+  const author = await getAuthorInfo(authorId);
 
   return {
     props: {
-      initialFeedback: feedback
+      initialFeedback: feedback,
+      siteOnPage: sites,
+      authorOnPage: author,
     },
     revalidate: 1
   }
@@ -33,7 +40,7 @@ export async function getStaticPaths() {
   };
 }
 
-export default function SiteFeedback({ initialFeedback }) {
+export default function SiteFeedback({ initialFeedback, siteOnPage, authorOnPage }) {
   const auth = useAuth();
   const user = auth?.user;
 
@@ -42,6 +49,8 @@ export default function SiteFeedback({ initialFeedback }) {
   const toast = useToast();
 
   const [allFeedback, setAllFeedback] = useState(initialFeedback);
+  const [theSite] = useState(siteOnPage);
+  const [theAuthor] = useState(authorOnPage);
 
   const onSubmit = () => {
     const newFeedback = {
@@ -65,36 +74,62 @@ export default function SiteFeedback({ initialFeedback }) {
       duration: 5000,
       isClosable: true
     });
-    // mutate('/api/sites', async (data) => { 
-    //   return {sites: [...data.sites, newSite] };
-    // }, false);
   };
 
-  return <Box display="flex" flexDirection="column" w="full" maxW="700px" margin="0 auto">
-    <Box as="form" onSubmit={onSubmit}>
-      <FormControl my={8} >
-        <FormLabel>Comment</FormLabel>
-        <Input ref={inputContent} type="comment" name="comment" />
-        <Button type="submit" background="#69aaac" color="#fdfdfd" my={3}
-          _hover={{ bg: "gray.900" }}
-          isDisabled={router.isFallback}
-        >
-          Add Comment
-        </Button>
-      </FormControl>
+  return (
+    <Box>
+      <Stack my={10} mx={5} alignItems="center">
+        <Stack width="full" maxW="70em">
+          {theSite && <SiteInfo theSite={theSite} theAuthor={theAuthor} />}
+          {/* {theSite && <Text as="h1">{theSite.name}</Text>} */}
+
+          <Box as="form" onSubmit={onSubmit}>
+            <FormControl my={8} >
+              <FormLabel>Comment</FormLabel>
+              <Input ref={inputContent} type="comment" name="comment" />
+              <Button type="submit" background="#69aaac" color="#fdfdfd" my={3}
+                _hover={{ bg: "gray.900" }}
+                isDisabled={router.isFallback}
+              >
+                Add Comment
+              </Button>
+            </FormControl>
+          </Box>
+          
+          {allFeedback && allFeedback.map((feedback) => (
+            <EachFeedback key={feedback.id} feedback={feedback} />
+          ))}
+        </Stack>
+      </Stack>  
     </Box>
-    
-    {allFeedback && allFeedback.map((feedback) => (
-      <EachFeedback key={feedback.id} feedback={feedback} />
-    ))}
-  </Box>
+  )
+}
+
+function SiteInfo({ theSite, theAuthor }) {
+
+  // console.log(siteId);
+
+  return (
+    <Box>
+      <Text as="h1" fontWeight="600" fontSize="3em">{theSite.name}</Text>
+      <NextLink href={theSite.url} passHref>
+        <Text cursor="pointer" as="a" target="_blank" fontWeight="600" fontSize="1em" color="gray.800"
+          _hover={{ color: "#69aaac" }}>{theSite.url}</Text>
+      </NextLink>
+      <Flex my={2} align="center">
+        <Avatar size="xs" mr={2} src={theAuthor.photoURL} />
+        <Text as="p" color="gray.700">Site created by {theAuthor.name}</Text>
+      </Flex>
+      
+    </Box> 
+  )
 }
 
 function EachFeedback({ feedback }) {
   
   return <Feedback 
-  key={feedback.id} 
-  author={feedback.author} 
-  text={feedback.text} 
-  createdAt={feedback.createdAt}/>
+    key={feedback.id} 
+    author={feedback.author} 
+    text={feedback.text} 
+    createdAt={feedback.createdAt}/>
 }
